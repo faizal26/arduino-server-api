7ed6582b845bcc852eb28cea77fb793efda7998c
Zharif Othman <zharifothman92@gmail.com>
2018-12-11 22:38:50 +0800

    add device token checking

--- functions/src/services/Notifications.js
+++ functions/src/services/Notifications.js
@@ -6,7 +6,7 @@ const url = "https://exp.host/--/api/v2/push/send";
 // query to user collection then get device token
 getDeviceToken = (arduinoId) => {
   return new Promise((resolve) => {
-    const refDeviceToken = admin.database().ref()
+    const refDeviceToken = admin.database().ref();
     const query = refDeviceToken.child('users').orderByChild('/arduinoId').equalTo(arduinoId);    
     query.once('value', (snapshot) => {
       const notes = snapshot.val();
@@ -20,30 +20,58 @@ getDeviceToken = (arduinoId) => {
 
 module.exports = {
   warning: (arduinoId) => {
-    getDeviceToken(arduinoId).then((deviceToken) => {
-      const bodyObject = { 
-        "to": deviceToken, // device token get from database
-        "title": "Warning Notification",
-        "body": "Get Your Car for Checkup!!"
-      };
+    getDeviceToken(arduinoId).then((tokens) => {
+      // console.log('Token:', deviceToken);
+      // const bodyObject = { 
+      //   "to": deviceToken, // device token get from database
+      //   "title": "Warning Notification",
+      //   "body": "Get Your Car for Checkup!!"
+      // };
   
-      return request({
-          url,
-          method: "POST",
-          json: true,
-          body: bodyObject
-      }, function (error, response, body){
+      // return request({
+      //     url,
+      //     method: "POST",
+      //     json: true,
+      //     body: bodyObject
+      // }, function (error, response, body){
+      //   if (error) {
+      //     console.log(error)
+      //   } else {
+      //     console.log(body);
+      //   }
+      // });
+
+      const payload = {
+        notification: {
+          title: 'Warning Notification',
+          body: `Get your car checkup!!`,
+          // icon: follower.photoURL
+        }
+      };
+
+      return admin.messaging().sendToDevice(tokens, payload);
+    })
+    .then((response) => {
+      // For each message check if there was an error.
+      const tokensToRemove = [];
+      response.results.forEach((result, index) => {
+        const error = result.error;
         if (error) {
-          console.log(error)
-        } else {
-          console.log(body);
+          console.error('Failure sending notification to', tokens[index], error);
+          // Cleanup the tokens who are not registered anymore.
+          // if (error.code === 'messaging/invalid-registration-token' ||
+          //     error.code === 'messaging/registration-token-not-registered') {
+          //   tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+          // }
         }
       });
+      // return Promise.all(tokensToRemove);
     })
   },
 
   danger: (arduinoId) => {
     getDeviceToken(arduinoId).then((deviceToken) => {
+      console.log('Token', deviceToken);
       const bodyObject = { 
         "to": deviceToken,
         "title": "Danger Notification",
